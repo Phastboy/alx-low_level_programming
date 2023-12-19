93cf1128809d81baadaf8162b6616b2359329c6a
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-12-19 06:21:48 +0000

    0x02-functions_nested_loops/0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+* main - function that prints a string
+*
+* Return: 0 if success, non-zero if otherwise
+*/
+
+# include <stdio.h>
+
+int main(void)
+{
+    printf("_putchar");
+    return (0);
+}
