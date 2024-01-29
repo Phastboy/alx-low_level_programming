433e00791d5e60c9a596688dd10abad0119b26c2
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-01-29 02:37:45 +0100

    2-print_strings.c

---
+++ 0x10-variadic_function/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between strings
+ * @n: number of strings passed to the function
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+    unsigned int i;
+    va_list args;
+    va_start(args, n);
+
+    for (i = 0; i < n; i++) {
+        if(!separator)
+        {
+            printf("%s", va_arg(args, char *));
+        }
+        else
+        {
+            if (i == n - 1)
+            {
+                printf("%s", va_arg(args, char *));
+            }
+            else
+            {
+                printf("%s%s", va_arg(args, char *), separator);
+            }
+        }
+    }
+    printf("\n");
+    va_end(args);
+}
